d5132ad66be801867592f58cf3a9dde1db9e5501
Sam Potter <sam_pot_97@hotmail.com>
2017-04-23 16:49:32 +0100

    Crafting uses hotbar aswell

--- LD-Game/Assets/Scripts/UI/ChestOverlay.cs
+++ LD-Game/Assets/Scripts/UI/ChestOverlay.cs
@@ -52,6 +52,17 @@ public class ChestOverlay : MonoBehaviour {
 					Count[slot.ID] = 1;
 			}
 		}
+
+		foreach (ItemSlot slot in PlayerInput.Main.mPerson.HotBar)
+		{
+			if (slot.ID != ItemID.None)
+			{
+				if (Count.ContainsKey(slot.ID))
+					Count[slot.ID]++;
+				else
+					Count[slot.ID] = 1;
+			}
+		}
 	}
 
 	public bool GiveItem(ItemID ID)
@@ -72,7 +83,20 @@ public class ChestOverlay : MonoBehaviour {
 
 	public void Consume(ItemID ID, int Count = 1)
 	{
+		if (Count <= 0)
+			return;
+
 		foreach (ItemSlot slot in Slots)
+		{
+			if (slot.ID == ID)
+			{
+				slot.SetID(ItemID.None);
+				if (--Count <= 0)
+					return;
+			}
+		}
+
+		foreach (ItemSlot slot in PlayerInput.Main.mPerson.HotBar)
 		{
 			if (slot.ID == ID)
 			{
